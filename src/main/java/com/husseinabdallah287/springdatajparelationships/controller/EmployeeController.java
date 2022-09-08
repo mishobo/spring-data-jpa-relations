03a783b693ef518b2ca577083c849a142bf66d90
mishobo <60912349+mishobo@users.noreply.github.com>
2022-09-08 10:29:29 +0300

    jasperReport service

--- src/main/java/com/husseinabdallah287/springdatajparelationships/controller/EmployeeController.java
+++ src/main/java/com/husseinabdallah287/springdatajparelationships/controller/EmployeeController.java
@@ -1,11 +1,13 @@
 package com.husseinabdallah287.springdatajparelationships.controller;
 
+import java.io.FileNotFoundException;
 import java.util.List;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.husseinabdallah287.springdatajparelationships.models.Employee;
 import com.husseinabdallah287.springdatajparelationships.repository.EmployeeRepository;
+import com.husseinabdallah287.springdatajparelationships.service.ReportService;
+
+import net.sf.jasperreports.engine.JRException;
 
 @RestController
 @RequestMapping("/api/employees/")
@@ -20,6 +25,9 @@ public class EmployeeController {
 	
 	@Autowired
 	private EmployeeRepository employeeRepository;
+	
+	@Autowired
+	private ReportService reportService;
 	
 	@PostMapping("/addEmployee")
 	public Employee createOrder(@Valid @RequestBody Employee employee ) {
@@ -29,6 +37,11 @@ public class EmployeeController {
     @GetMapping("/getEmployees")
     public List<Employee> getEmployees() {
         return employeeRepository.findAll();
+    }
+    
+    @GetMapping("/report/{format}")
+    public String generateReport(@PathVariable String format) throws FileNotFoundException, JRException {
+        return reportService.exportReport(format);
     }
 
 }
---
+++ src/main/java/com/husseinabdallah287/springdatajparelationships/service/ReportService.java
@@ -0,0 +1,53 @@
+package com.husseinabdallah287.springdatajparelationships.service;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+import com.husseinabdallah287.springdatajparelationships.models.Employee;
+import com.husseinabdallah287.springdatajparelationships.repository.EmployeeRepository;
+
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
+@Service
+public class ReportService{
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	public String exportReport(String reportFormat) throws FileNotFoundException, JRException {
+		String path = "E:\\LCT\\version 2\\pdf statements";
+		
+		List<Employee> employees = employeeRepository.findAll();
+		
+		File file = ResourceUtils.getFile("classpath: ReportService");
+		JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+		JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(employees);
+		Map<String, Object> parameters = new HashMap<>();
+		parameters.put("createdBy", "Java Techie");
+		JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+        if (reportFormat.equalsIgnoreCase("html")) {
+            JasperExportManager.exportReportToHtmlFile(jasperPrint, path + "\\employees.html");
+        }
+        if (reportFormat.equalsIgnoreCase("pdf")) {
+            JasperExportManager.exportReportToPdfFile(jasperPrint, path + "\\employees.pdf");
+        }
+		
+		
+		return "report generated in path : " + path;
+	}
+	
+	
+}
